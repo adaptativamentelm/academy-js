099a4d6790378a0e856116f2109342bcf1ad9962
Luis Miranda <miranda.geldres@gmail.com>
2020-06-14 23:51:17 -0400

    15 06 20 html vacio y mas validaciones

---
+++ 150620/app.js
@@ -0,0 +1,20 @@
+let jsonObject = {
+    name: "Luis",
+    lastName: "Miranda",
+    rut: "23745869-9"
+};
+
+let img = document.getElementById('img');
+img.style.width = "100%";
+
+function showPass() {
+
+}
+
+function validate() {
+
+}
+
+function getInformation() {
+
+}
