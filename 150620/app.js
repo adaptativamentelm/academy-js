9246151d8edabe17a944b4c5862f9c5cb7a5c0de
AndreaMiranda1 <andrea@adaptativamente.cl>
2020-06-17 10:16:15 -0400

    avance lunes

--- 150620/app.js
+++ 150620/app.js
@@ -1,5 +1,25 @@
 const jsonObject = {
-    name: "Luis",
-    lastName: "Miranda",
-    rut: "23745869-9"
+    name: "Chinchilla",
+    lastName: "Psico",
+    rut: "18122171-2"
 };
+document.getElementById('header').style.width = '100%';
+function show() {
+    let pass = document.getElementById("pass");
+    if (pass.type === "password") {
+      pass.type = "text";
+    } else {
+      pass.type = "password";
+    }
+}
+function section1() {
+    let userGood = `${jsonObject.name[0]}`+`${jsonObject.lastName}`;
+    let passGood = jsonObject.rut.substring(0,4);
+    let user = document.getElementById('user').value;
+    let pass = document.getElementById('pass');
+    if (user.toLowerCase() == userGood.toLowerCase() && pass.value == passGood) {
+        console.log(" contraseña y usuario correctos");
+    }else{
+      console.log("usuario y/o contraseña incorrectos", userGood, passGood);
+    } 
+}
