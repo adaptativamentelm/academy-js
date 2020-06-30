eacf062e3dac512bb50bc9e552d40e8b4cfb63c2
Luis Miranda <miranda.geldres@gmail.com>
2020-06-29 20:57:03 -0400

    removedor de tildes, comentarios, orden y protocolo de doble comilla

--- 150620/app.js
+++ 150620/app.js
@@ -1,52 +1,52 @@
-const jsonObject = {
-    name: "Luis",
-    lastName: "Miranda",
-    rut: "23745869-9"
-};
+// HEADER
+document.getElementById("img").style.width = "100%";
+// HEADER
 
-let img = document.getElementById('img');
-img.style.width = "100%";
-let user = document.getElementById('user');
-let pass = document.getElementById('pass');
-let date = document.getElementById('date');
-let inputsRadio = document.getElementsByClassName('radio');
-let inputsCheckbox = document.getElementsByClassName('checkbox');
+// SECTION 1
+const jsonObject = {
+    "name": "Luis",
+    "lastName": "Miranda",
+    "rut": "23745869-9"
+};
+let user = document.getElementById("user");
+let pass = document.getElementById("pass");
 
 function showPass() {
-    pass.type = pass.type === 'text' ? pass.type = 'password' : 'text';
+    pass.type = pass.type === "text" ? pass.type = "password" : "text";
 }
 
 function validate() {
     if (user.value && pass.value) {
-        if (user.value === `${jsonObject.name.substr(0, 1).toLowerCase()}${jsonObject.lastName.toLowerCase()}` && pass.value === `${jsonObject.rut.replace('-', '').substr(0, 4)}`) {
-            alert('Credenciales correctas!');
+        if (user.value === removeDiacritics(`${jsonObject.name[0]}${jsonObject.lastName}`.toLowerCase()) && pass.value === `${jsonObject.rut.replace("-", "").substr(0, 4)}`) {
+            alert("Credenciales correctas!");
             resetInputs();
         } else {
-            alert('Credenciales incorrectas!');
+            alert("Credenciales incorrectas!");
         }
     } else {
-        alert('No se ingresaron credenciales');
+        alert("No se ingresaron credenciales");
     }
 }
 
-function resetInputs() {
-    user.value = '';
-    pass.value = '';
+function removeDiacritics(str) {
+    return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
 }
+
+function resetInputs() {
+    user.value = "";
+    pass.value = "";
+}
+// SECTION 1
+
+// SECTION 2
+let date = document.getElementById("date");
+let inputsRadio = document.getElementsByClassName("radio");
+let inputsCheckbox = document.getElementsByClassName("checkbox");
 
 function getInformation() {
-    let response = {
-        date: date.value,
-        choice: Array.from(inputsRadio).filter(data => data.checked).map(data => data.value)[0],
-        interests: Array.from(inputsCheckbox).filter(data => data.checked).map(data => data.value)
-    };
-    console.log('Respuesta:', response);    
-}
-
-function getInformation2() {
     let response = {};
     response.date = date.value;
-    let choice = '';
+    let choice = "";
     for (let x = 0; x < inputsRadio.length; x++) {
         if (inputsRadio[x].checked) {
             choice = inputsRadio[x].value;
@@ -61,5 +61,15 @@ function getInformation2() {
         }
     }
     response.interests = interests;
-    console.log('Respuesta:', response);
+    console.log("Respuesta:", response);
 }
+
+function getInformation2() {
+    let response = {
+        "date": date.value,
+        "choice": Array.from(inputsRadio).filter(data => data.checked).map(data => data.value)[0],
+        "interests": Array.from(inputsCheckbox).filter(data => data.checked).map(data => data.value)
+    };
+    console.log("Respuesta:", response);
+}
+// SECTION2
