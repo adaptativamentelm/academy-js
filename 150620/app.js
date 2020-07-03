40379fb50442bb34f5f702520be6537368d96798
Luis Miranda <miranda.geldres@gmail.com>
2020-07-03 11:07:38 -0400

    tarea root

--- 150620/app.js
+++ 150620/app.js
@@ -11,13 +11,33 @@ const jsonObject = {
 let user = document.getElementById("user");
 let pass = document.getElementById("pass");
 
-function showPass() {
-    pass.type = pass.type === "text" ? pass.type = "password" : "text";
+function showPass1() {
+    if (pass.type === "text") {
+        pass.type = "password";
+    } else {
+        pass.type = "text";
+    }
 }
 
+function showPass2() {
+    pass.type = pass.type === "text" ? "password" : "text";
+}
+
+// Scarlett Platón
+// SPlatón `${jsonObject.name[0]}${jsonObject.lastName}`
+// splatón `${jsonObject.name[0]}${jsonObject.lastName}`.toLowerCase()
+// splaton removeDiacritics(`${jsonObject.name[0]}${jsonObject.lastName}`.toLowerCase())
+
+// 23745869-9
+// 237458699 `${jsonObject.rut.replace("-", "")
+// 2374 `${jsonObject.rut.replace("-", "").substr(0, 4)}`
+
 function validate1() {
+    // console.log(`${jsonObject.name[0]}${jsonObject.lastName}`);
+    // console.log(`${jsonObject.name[0]}${jsonObject.lastName}`.toLowerCase());
+    // console.log(removeDiacritics(`${jsonObject.name[0]}${jsonObject.lastName}`.toLowerCase()));
     if (user.value && pass.value) {
-        if (user.value === removeDiacritics(`${jsonObject.name[0]}${jsonObject.lastName}`.toLowerCase()) && pass.value === `${jsonObject.rut.replace("-", "").substr(0, 4)}`) {
+        if (removeDiacritics(user.value.toLowerCase()) === removeDiacritics(`${jsonObject.name[0]}${jsonObject.lastName}`.toLowerCase()) && pass.value === `${jsonObject.rut.replace("-", "").substr(0, 4)}`) {
             okResetInputs();
         } else {
             alert("Credenciales incorrectas!");
@@ -28,7 +48,7 @@ function validate1() {
 }
 
 function validate2() {
-	user.value && pass.value ? user.value === removeDiacritics(`${jsonObject.name[0]}${jsonObject.lastName}`.toLowerCase()) && pass.value === `${jsonObject.rut.replace("-", "").substr(0, 4)}` ? okResetInputs() : alert("Credenciales incorrectas!") : alert("Falta ingresar usuario y/o contraseña");
+	user.value && pass.value ? removeDiacritics(user.value.toLowerCase()) === removeDiacritics(`${jsonObject.name[0]}${jsonObject.lastName}`.toLowerCase()) && pass.value === `${jsonObject.rut.replace("-", "").substr(0, 4)}` ? okResetInputs() : alert("Credenciales incorrectas!") : alert("Falta ingresar usuario y/o contraseña");
 }
 
 function removeDiacritics(str) {
@@ -51,14 +71,12 @@ let inputsCheckbox = document.getElementsByClassName("checkbox");
 function printInformation1() {
     let response = {};
     response.date = date.value;
-    let choice = "";
     for (let x = 0; x < inputsRadio.length; x++) {
         if (inputsRadio[x].checked) {
-            choice = inputsRadio[x].value;
+            response.choice = inputsRadio[x].value;
             break;
         }
     }
-    response.choice = choice;
     let interests = [];
     for (let x = 0; x < inputsCheckbox.length; x++) {
         if (inputsCheckbox[x].checked) {
