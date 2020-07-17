fbb8b6d92c74e261be0b5c68f1e7bc21349e7ff0
Luis Miranda <miranda.geldres@gmail.com>
2020-07-17 11:11:36 -0400

    json de ejemplo

--- 200717/select/app.js
+++ 200717/select/app.js
@@ -1,6 +1,4 @@
-// let option1 = document.getElementById("option1");
-// let option2 = document.getElementById("option2");
-// let option3 = document.getElementById("option3");
+let options = document.getElementsByClassName('option');
 
 let selectElement = document.getElementById('select');
 console.log('Elementos contenidos en el select:', selectElement.options);
@@ -9,3 +7,49 @@ function myFunction() {
     console.log('Índice de la opción seleccionada:', selectElement.selectedIndex);
     console.log('Valor de la opción seleccionada:', selectElement.options[selectElement.selectedIndex].value);
 }
+
+// selectElement = {
+//     "id": 'select',
+//     "options": [
+//         {
+//             "value": '',
+//             "selected": true,
+//             "disabled": true,
+//             "innerHTML": 'Elige opción',
+//             "classList": [
+//                 'option',
+//             ]
+//         },
+//         {
+//             "id": 'option1',
+//             "value": 'Opción 1',
+//             "selected": false,
+//             "disabled": false,
+//             "innerHTML": 'Opción 1',
+//             "classList": [
+//                 'option'
+//             ]
+//         },
+//         {
+//             "id": 'option2',
+//             "value": 'Opción 2',
+//             "selected": false,
+//             "disabled": false,
+//             "innerHTML": 'Opción 2',
+//             "classList": [
+//                 'option'
+//             ]
+//         },
+//         {
+//             "id": 'option3',
+//             "value": 'Opción 3',
+//             "selected": false,
+//             "disabled": false,
+//             "innerHTML": 'Opción 3',
+//             "classList": [
+//                 'option'
+//             ]
+//         }
+//     ],
+//     "selectedIndex": 2
+// };
