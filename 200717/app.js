ffa8f3fb4fe427d52e3b96c89460c0013b88773b
scarplaton <51921205+scarplaton@users.noreply.github.com>
2020-07-29 10:47:02 -0400

    avance tarea 20/07/17

--- 200717/app.js
+++ 200717/app.js
@@ -1,2 +1,10 @@
 let bd_enteros = [1,2,3,4,5,6,7,8,9,10];
 let bd_decimos = [10,20,30,40,50,60,70,80,90,100];
+
+
+
+ function selector(){
+     var x = document.getElementById('mySelect').value;     
+     console.log(x);
+ }
+ 
