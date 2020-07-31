9d75edb77f7486538c54260474bc71847eb5a6c1
AndreaMiranda1 <andrea@adaptativamente.cl>
2020-07-31 13:02:33 -0400

    subiendo avances formulario

--- 200729-tareaCallbackHtml/app.js
+++ 200729-tareaCallbackHtml/app.js
@@ -8,8 +8,38 @@ let genero = '';
 let checks = document.getElementsByClassName('checkbox');
 let hobbies = [];
 let save = document.getElementById('save');
+showPass.addEventListener('click', showPassf);
+function showPassf() {
+    pass.type = pass.type === "text" ? "password" : "text";
+}
 
 const getDataForm = () => { 
     console.log('funciona');
+    let response = {};
+    response.name = name.value;
+    response.phone = phone.value;
+    response.user = user.value;
+    response.pass = pass.value;
+    for (let x = 0; x < radios.length; x++) {
+        if (radios[x].checked) {
+            response.genero = radios[x].value;
+            break;
+        }
+    }
+    for (let x = 0; x < checks.length; x++) {
+        if (checks[x].checked) {
+            hobbies.push(checks[x].value);
+        }
+    }
+    
+    response.hobbies = hobbies;
+    response.name && response.phone && response.user && response.pass && response.genero && response.hobbies.length ? console.log("Respuesta:", response) : console.log("Faltan datos");
+
 } // Esta función será llamada en el evento click del botón "Guardar datos"
-save.addEventListener('click', getDataForm)
+
+
+
+
+save.addEventListener('click', getDataForm);
+
+
