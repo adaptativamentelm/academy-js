2b3b8f5b45dd05cbdb5290f7f186dbf730dce19f
Luis Miranda <miranda.geldres@gmail.com>
2020-08-05 08:35:22 -0400

    tarea 2 callback

---
+++ 200805-tarea2CallbackHtml/app.js
@@ -0,0 +1,7 @@
+let fullscreenDiv = document.getElementById('fullscreen');
+let activateFs = document.getElementById('activatefs');
+let image = document.getElementsByTagName('img');
+let base = document.getElementById('base');
+let exponent = document.getElementById('exponent');
+let reset = document.getElementById('reset');
+let resultPow = document.getElementById('resultPow');
