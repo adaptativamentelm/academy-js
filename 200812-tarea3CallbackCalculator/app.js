bc4766326c01585309be87a40b91ca3822419155
AndreaMiranda1 <andrea@adaptativamente.cl>
2020-08-31 11:48:03 -0400

    casi listoo

--- 200812-tarea3CallbackCalculator/app.js
+++ 200812-tarea3CallbackCalculator/app.js
@@ -7,3 +7,103 @@ let multiplicar = document.getElementById('multiplicar');
 let dividir = document.getElementById('dividir');
 let potencia = document.getElementById('potencia');
 let raiz = document.getElementById('raiz');
+
+function sum() {
+    try {
+        if (num1.value && num2.value) {
+            res.innerHTML = parseInt(num1.value) + parseInt(num2.value);
+        } else {
+            error();
+            alert('Error');
+        }
+    } catch {
+        error();
+        alert('Error');
+    }    
+}
+
+function rest() {
+    try {
+        if (num1.value && num2.value) {
+            res.innerHTML = num1.value - num2.value;
+        } else {
+            error();
+            alert('Error');
+        }
+    } catch {
+        error();
+        alert('Error');
+    }
+}
+
+function multi() {
+    try {
+        if (num1.value && num2.value) {
+            res.innerHTML = num1.value * num2.value;
+        } else {
+            error();
+            alert('Error');
+        }
+    } catch {
+        error();
+        alert('Error');
+    }    
+}
+
+function div() {
+    try {
+        if (num1.value && num2.value) {
+            res.innerHTML = num1.value / num2.value;
+        } else {
+            error();
+            alert('Error');
+        }
+    } catch {
+        error();
+        alert('Error');
+    }    
+}
+
+function pow () {
+    try {
+        if (num1.value && num2.value) {
+            res.innerHTML = Math.pow(num1.value, num2.value);
+        } else {
+            error();
+            alert('Error');
+        }
+    } catch {
+        error();
+        alert('Error');
+    }
+}
+
+function raiznums() {
+    try {
+        if (num1.value && num2.value) {
+            let indice = 1/num2.value;
+            res.innerHTML = Math.pow(num1.value, indice);
+        } else {
+            error();
+            alert('Error');
+        }
+    } catch {
+        error();
+        alert('Error');
+    }
+}
+
+function error() {
+    num1.value = '';
+    num2.value = '';
+    res.innerHTML = '';
+    num1.focus();
+}
+
+res.addEventListener('click', error);
+sumar.addEventListener('click', sum);
+restar.addEventListener('click', rest);
+multiplicar.addEventListener('click', multi);
+dividir.addEventListener('click', div);
+potencia.addEventListener('click', pow);
+raiz.addEventListener('click', raiznums);
