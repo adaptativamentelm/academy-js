9ef77325e2e59ff07d227729f7b760bef1a3af5a
Luis Miranda <miranda.geldres@gmail.com>
2020-09-11 21:47:15 -0300

    repaso segunda forma callback addeventlistener

--- 200904-repaso/app.js
+++ 200904-repaso/app.js
@@ -9,7 +9,7 @@ let title = document.getElementsByTagName('h1');
 
 title[0].innerHTML = 'andrea!';
 
-print.addEventListener('click', () => {
+const imprimir = () => {
     console.log('name:', name.value);
     console.log('pass:', pass.value);
 
@@ -24,7 +24,26 @@ print.addEventListener('click', () => {
             console.log('comida:', food_list[i].value);
         }
     }
-});
+}
+
+print.addEventListener('click', imprimir);
+
+// print.addEventListener('click', () => {
+//     console.log('name:', name.value);
+//     console.log('pass:', pass.value);
+
+//     for (let x = 0; x < form_info.length; x++) {
+//         if (form_info[x].checked) {
+//             console.log('radios:', form_info[x].value);
+//         }
+//     }
+
+//     for (let i = 0; i < food_list.length; i++) {
+//         if (food_list[i].checked) {
+//             console.log('comida:', food_list[i].value);
+//         }
+//     }
+// });
 
 // let andrea = {
 //     "name": 'Andreabb',
@@ -41,6 +60,406 @@ print.addEventListener('click', () => {
 // console.log('andrea name1:', andrea.name);
 // console.log('andrea name2:', andrea['name']);
 // andrea.print();
+
+// let bbs1 = [
+//     andrea, scar
+// ];
+
+// let bbs2 = [{
+//         "name": 'Andreabb',
+//         "lastname": 'Castro',
+//         "print": () => console.log('hola soy andrea')
+//     },
+//     {
+//         "name": 'Scar',
+//         "lastname": 'Pla',
+//         "print": () => console.log('hola soy scar')
+//     }
+// ];
+
+// let bbs1 = [
+//     andrea, scar
+// ];
+
+// let bbs2 = [{
+//         "name": 'Andreabb',
+//         "lastname": 'Castro',
+//         "print": () => console.log('hola soy andrea')
+//     },
+//     {
+//         "name": 'Scar',
+//         "lastname": 'Pla',
+//         "print": () => console.log('hola soy scar')
+//     }
+// ];
+
+// let bbs1 = [
+//     andrea, scar
+// ];
+
+// let bbs2 = [{
+//         "name": 'Andreabb',
+//         "lastname": 'Castro',
+//         "print": () => console.log('hola soy andrea')
+//     },
+//     {
+//         "name": 'Scar',
+//         "lastname": 'Pla',
+//         "print": () => console.log('hola soy scar')
+//     }
+// ];
+
+// let bbs1 = [
+//     andrea, scar
+// ];
+
+// let bbs2 = [{
+//         "name": 'Andreabb',
+//         "lastname": 'Castro',
+//         "print": () => console.log('hola soy andrea')
+//     },
+//     {
+//         "name": 'Scar',
+//         "lastname": 'Pla',
+//         "print": () => console.log('hola soy scar')
+//     }
+// ];
+
+// let bbs1 = [
+//     andrea, scar
+// ];
+
+// let bbs2 = [{
+//         "name": 'Andreabb',
+//         "lastname": 'Castro',
+//         "print": () => console.log('hola soy andrea')
+//     },
+//     {
+//         "name": 'Scar',
+//         "lastname": 'Pla',
+//         "print": () => console.log('hola soy scar')
+//     }
+// ];
+
+// let bbs1 = [
+//     andrea, scar
+// ];
+
+// let bbs2 = [{
+//         "name": 'Andreabb',
+//         "lastname": 'Castro',
+//         "print": () => console.log('hola soy andrea')
+//     },
+//     {
+//         "name": 'Scar',
+//         "lastname": 'Pla',
+//         "print": () => console.log('hola soy scar')
+//     }
+// ];
+
+// let bbs1 = [
+//     andrea, scar
+// ];
+
+// let bbs2 = [{
+//         "name": 'Andreabb',
+//         "lastname": 'Castro',
+//         "print": () => console.log('hola soy andrea')
+//     },
+//     {
+//         "name": 'Scar',
+//         "lastname": 'Pla',
+//         "print": () => console.log('hola soy scar')
+//     }
+// ];
+
+// let bbs1 = [
+//     andrea, scar
+// ];
+
+// let bbs2 = [{
+//         "name": 'Andreabb',
+//         "lastname": 'Castro',
+//         "print": () => console.log('hola soy andrea')
+//     },
+//     {
+//         "name": 'Scar',
+//         "lastname": 'Pla',
+//         "print": () => console.log('hola soy scar')
+//     }
+// ];
+
+// let bbs1 = [
+//     andrea, scar
+// ];
+
+// let bbs2 = [{
+//         "name": 'Andreabb',
+//         "lastname": 'Castro',
+//         "print": () => console.log('hola soy andrea')
+//     },
+//     {
+//         "name": 'Scar',
+//         "lastname": 'Pla',
+//         "print": () => console.log('hola soy scar')
+//     }
+// ];
+
+// let bbs1 = [
+//     andrea, scar
+// ];
+
+// let bbs2 = [{
+//         "name": 'Andreabb',
+//         "lastname": 'Castro',
+//         "print": () => console.log('hola soy andrea')
+//     },
+//     {
+//         "name": 'Scar',
+//         "lastname": 'Pla',
+//         "print": () => console.log('hola soy scar')
+//     }
+// ];
+
+// let bbs1 = [
+//     andrea, scar
+// ];
+
+// let bbs2 = [{
+//         "name": 'Andreabb',
+//         "lastname": 'Castro',
+//         "print": () => console.log('hola soy andrea')
+//     },
+//     {
+//         "name": 'Scar',
+//         "lastname": 'Pla',
+//         "print": () => console.log('hola soy scar')
+//     }
+// ];
+
+// let bbs1 = [
+//     andrea, scar
+// ];
+
+// let bbs2 = [{
+//         "name": 'Andreabb',
+//         "lastname": 'Castro',
+//         "print": () => console.log('hola soy andrea')
+//     },
+//     {
+//         "name": 'Scar',
+//         "lastname": 'Pla',
+//         "print": () => console.log('hola soy scar')
+//     }
+// ];
+
+// let bbs1 = [
+//     andrea, scar
+// ];
+
+// let bbs2 = [{
+//         "name": 'Andreabb',
+//         "lastname": 'Castro',
+//         "print": () => console.log('hola soy andrea')
+//     },
+//     {
+//         "name": 'Scar',
+//         "lastname": 'Pla',
+//         "print": () => console.log('hola soy scar')
+//     }
+// ];
+
+// let bbs1 = [
+//     andrea, scar
+// ];
+
+// let bbs2 = [{
+//         "name": 'Andreabb',
+//         "lastname": 'Castro',
+//         "print": () => console.log('hola soy andrea')
+//     },
+//     {
+//         "name": 'Scar',
+//         "lastname": 'Pla',
+//         "print": () => console.log('hola soy scar')
+//     }
+// ];
+
+// let bbs1 = [
+//     andrea, scar
+// ];
+
+// let bbs2 = [{
+//         "name": 'Andreabb',
+//         "lastname": 'Castro',
+//         "print": () => console.log('hola soy andrea')
+//     },
+//     {
+//         "name": 'Scar',
+//         "lastname": 'Pla',
+//         "print": () => console.log('hola soy scar')
+//     }
+// ];
+
+// let bbs1 = [
+//     andrea, scar
+// ];
+
+// let bbs2 = [{
+//         "name": 'Andreabb',
+//         "lastname": 'Castro',
+//         "print": () => console.log('hola soy andrea')
+//     },
+//     {
+//         "name": 'Scar',
+//         "lastname": 'Pla',
+//         "print": () => console.log('hola soy scar')
+//     }
+// ];
+
+// let bbs1 = [
+//     andrea, scar
+// ];
+
+// let bbs2 = [{
+//         "name": 'Andreabb',
+//         "lastname": 'Castro',
+//         "print": () => console.log('hola soy andrea')
+//     },
+//     {
+//         "name": 'Scar',
+//         "lastname": 'Pla',
+//         "print": () => console.log('hola soy scar')
+//     }
+// ];
+
+// let bbs1 = [
+//     andrea, scar
+// ];
+
+// let bbs2 = [{
+//         "name": 'Andreabb',
+//         "lastname": 'Castro',
+//         "print": () => console.log('hola soy andrea')
+//     },
+//     {
+//         "name": 'Scar',
+//         "lastname": 'Pla',
+//         "print": () => console.log('hola soy scar')
+//     }
+// ];
+
+// let bbs1 = [
+//     andrea, scar
+// ];
+
+// let bbs2 = [{
+//         "name": 'Andreabb',
+//         "lastname": 'Castro',
+//         "print": () => console.log('hola soy andrea')
+//     },
+//     {
+//         "name": 'Scar',
+//         "lastname": 'Pla',
+//         "print": () => console.log('hola soy scar')
+//     }
+// ];
+
+// let bbs1 = [
+//     andrea, scar
+// ];
+
+// let bbs2 = [{
+//         "name": 'Andreabb',
+//         "lastname": 'Castro',
+//         "print": () => console.log('hola soy andrea')
+//     },
+//     {
+//         "name": 'Scar',
+//         "lastname": 'Pla',
+//         "print": () => console.log('hola soy scar')
+//     }
+// ];
+
+// let bbs1 = [
+//     andrea, scar
+// ];
+
+// let bbs2 = [{
+//         "name": 'Andreabb',
+//         "lastname": 'Castro',
+//         "print": () => console.log('hola soy andrea')
+//     },
+//     {
+//         "name": 'Scar',
+//         "lastname": 'Pla',
+//         "print": () => console.log('hola soy scar')
+//     }
+// ];
+
+// let bbs1 = [
+//     andrea, scar
+// ];
+
+// let bbs2 = [{
+//         "name": 'Andreabb',
+//         "lastname": 'Castro',
+//         "print": () => console.log('hola soy andrea')
+//     },
+//     {
+//         "name": 'Scar',
+//         "lastname": 'Pla',
+//         "print": () => console.log('hola soy scar')
+//     }
+// ];
+
+// let bbs1 = [
+//     andrea, scar
+// ];
+
+// let bbs2 = [{
+//         "name": 'Andreabb',
+//         "lastname": 'Castro',
+//         "print": () => console.log('hola soy andrea')
+//     },
+//     {
+//         "name": 'Scar',
+//         "lastname": 'Pla',
+//         "print": () => console.log('hola soy scar')
+//     }
+// ];
+
+// let bbs1 = [
+//     andrea, scar
+// ];
+
+// let bbs2 = [{
+//         "name": 'Andreabb',
+//         "lastname": 'Castro',
+//         "print": () => console.log('hola soy andrea')
+//     },
+//     {
+//         "name": 'Scar',
+//         "lastname": 'Pla',
+//         "print": () => console.log('hola soy scar')
+//     }
+// ];
+
+// let bbs1 = [
+//     andrea, scar
+// ];
+
+// let bbs2 = [{
+//         "name": 'Andreabb',
+//         "lastname": 'Castro',
+//         "print": () => console.log('hola soy andrea')
+//     },
+//     {
+//         "name": 'Scar',
+//         "lastname": 'Pla',
+//         "print": () => console.log('hola soy scar')
+//     }
+// ];
 
 // let bbs1 = [
 //     andrea, scar
