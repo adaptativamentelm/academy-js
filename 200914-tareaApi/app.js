c9872abcccf1bc48ffd7b6b02a08894e6865ffe8
AndreaMiranda1 <andrea@adaptativamente.cl>
2020-09-15 15:54:14 -0400

    ya luchito, ahi están las img

--- 200914-tareaApi/app.js
+++ 200914-tareaApi/app.js
@@ -1,5 +1,6 @@
 const url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=iA435kVAfBUEHbkCpcZrSiklgadN9LAUU3xz5Bvc';
 let send = document.getElementById('click');
+let div = document.getElementById('container');
 
 const func2 = async () => {
 
@@ -8,7 +9,12 @@ const func2 = async () => {
         let response = await fetch(url);
         let data = await response.json();
         for (let i = 0; i < 5; i++) {
-            console.log(data.photos[i]);     
+            let img_url = data.photos[i].img_src;
+            let newImg = document.createElement('img');
+            newImg.style.maxWidth = '10em';
+            newImg.style.padding = '1em';
+            newImg.src = img_url;
+            div.appendChild(newImg);   
         }
         console.log('data', data);
     } catch (e) {
@@ -16,5 +22,4 @@ const func2 = async () => {
     }
 
 }
-
 send.addEventListener('click', func2);
