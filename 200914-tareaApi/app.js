e357a847f421ab658700faf3ffafc9f75d9d8c8a
Andrea Castro <ap.castrocastellon@gmail.com>
2020-09-16 23:35:35 -0300

    tarea api parte II

--- 200914-tareaApi/app.js
+++ 200914-tareaApi/app.js
@@ -2,25 +2,39 @@ let url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1
 
 let get_button = document.getElementById('btn');
 let data = [];
-
+let div = document.getElementById('show_data');
 
 const get_data = async () => {
     try {
         let response = await fetch(url);
         data = await response.json();
-        console.log(typeof data);
+        // console.log(typeof data);
         // console.log('data', data);
         print_data(data)
+        
 
     } catch (e) {
         console.log(e);
     }
 }
+
 const print_data = (data) => {
     let all_photos = data.photos;
-    for ( let i = 0 ; i < 5 ; i++ ){
-        console.log(all_photos[i])
+    for ( let i = 0 ; i < 60 ; i++ ){
+        if (i == 10 | i == 20 | i == 30 | i == 40 | i == 50 ){
+            // console.log(all_photos[i])
+            print_on_html(all_photos[i].img_src);
+            console.log(`photo ${i}`, all_photos[i].img_src);
+        }
     };
 };
+
+const print_on_html = (x) => {
+    let new_elem = document.createElement('img');
+    new_elem.src = x;
+    new_elem.width = 200;
+    new_elem.height = 200;
+    div.appendChild(new_elem);
+} 
 
 get_button.addEventListener('click', get_data);
