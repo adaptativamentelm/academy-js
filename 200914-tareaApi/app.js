db6e7d7ff15367db310b92979af00d5483d1740f
Andrea Castro <ap.castrocastellon@gmail.com>
2020-09-16 22:07:32 -0300

    api parte I

--- 200914-tareaApi/app.js
+++ 200914-tareaApi/app.js
@@ -1,1 +1,26 @@
 let url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=iA435kVAfBUEHbkCpcZrSiklgadN9LAUU3xz5Bvc';
+
+let get_button = document.getElementById('btn');
+let data = [];
+
+
+const get_data = async () => {
+    try {
+        let response = await fetch(url);
+        data = await response.json();
+        console.log(typeof data);
+        // console.log('data', data);
+        print_data(data)
+
+    } catch (e) {
+        console.log(e);
+    }
+}
+const print_data = (data) => {
+    let all_photos = data.photos;
+    for ( let i = 0 ; i < 5 ; i++ ){
+        console.log(all_photos[i])
+    };
+};
+
+get_button.addEventListener('click', get_data);
