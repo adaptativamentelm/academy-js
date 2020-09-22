e96ec36d27b43b966730d66937f4819599581bc8
scarplaton <51921205+scarplaton@users.noreply.github.com>
2020-09-21 23:48:40 -0300

    tarea 21/09

--- 200921-tareaApiDos/app.js
+++ 200921-tareaApiDos/app.js
@@ -1,2 +1,21 @@
 let get = 'https://portal-be.adaptativamente.cl/reporteria/academy/users';
 let post = 'https://portal-be.adaptativamente.cl/reporteria/academy/user/id';
+let pares = [];
+let impares= [];
+let data = [];
+
+const getData = async () => {
+    let response = await fetch(get);
+    let data = await response.json();
+    console.log(data)
+}
+getData();
+
+// for( i = 0; i < data; i++){
+//     console.log()
+// }
+
+const draw = () => {
+    let nombre = document.createElement('p')
+    
+}
