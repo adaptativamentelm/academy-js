ba0b43aa2e6d679497cd8579fc08ee8e7131e1fa
Dani Olivares <dany.olivaresl@gmail.com>
2020-09-21 23:45:10 -0300

    homework two api

--- 200921-tareaApiDos/app.js
+++ 200921-tareaApiDos/app.js
@@ -1,2 +1,31 @@
 let get = 'https://portal-be.adaptativamente.cl/reporteria/academy/users';
 let post = 'https://portal-be.adaptativamente.cl/reporteria/academy/user/id';
+
+let show = document.getElementById('show');
+let pairs = document.getElementById('pairs');
+let odd = document.getElementById('odd');
+
+const getUrl = async () => {
+    try {
+        let response = await fetch(get);
+        // console.log(response);
+        data = await response.json();
+        // console.log(data);
+        traveling(data); 
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+const traveling = (n) =>{    
+    for( let i = 0; i<n.length; i++) {
+        // console.log(n[i].id)
+        if (n[i].id %2 == 0) {
+            pairs.innerHTML += `<p>${n[i].name}</p>`;            
+        } else {
+            odd.innerHTML += `<p>${n[i].name}</p>`
+        }
+    }
+};
+
+show.addEventListener('click', getUrl);
