3c2b009bade65209cb6000a12b26927336c10c35
Luis Miranda <miranda.geldres@gmail.com>
2020-09-21 23:14:12 -0300

    avance

--- 200921-tareaApiDos/app.js
+++ 200921-tareaApiDos/app.js
@@ -1,2 +1,31 @@
 let get = 'https://portal-be.adaptativamente.cl/reporteria/academy/users';
 let post = 'https://portal-be.adaptativamente.cl/reporteria/academy/user/id';
+let data = [];
+let odds = document.getElementById('odd');
+let evens = document.getElementById('even');
+
+const getData = async () => {
+    const response = await fetch(get);
+    data = await response.json();
+    console.log('data', data);
+    processing();
+};
+
+const processing = () => {
+    for (let x = 0; x < data.length; x++) {
+        let odd = data[x].id % 2 ? true : false;
+        drawing(data[x].name, odd);
+    }
+};
+
+const drawing = (str, isOdd) => {
+    let h2 = document.createElement('h2');
+    h2.innerHTML = str;
+    if (isOdd) {
+        odds.appendChild(h2);
+    } else {
+        evens.appendChild(h2);
+    }
+};
+
+getData();
