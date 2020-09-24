397808f21b6b1a8ac852ade1923eec89f9c0853a
AndreaMiranda1 <andrea@adaptativamente.cl>
2020-09-24 19:51:34 -0400

    luchito ta listo, pero hay codigo repetido y estoy pensando como hacerlo mas chico, como tu amor por mi ajajajaj

--- 200921-tareaApiDos/app.js
+++ 200921-tareaApiDos/app.js
@@ -1,26 +1,38 @@
 let get = 'https://portal-be.adaptativamente.cl/reporteria/academy/users';
 let post = 'https://portal-be.adaptativamente.cl/reporteria/academy/user/id';
 let data = [];
-let odds = document.getElementById('odd');
-let evens = document.getElementById('even');
+let idUser = document.getElementById('idUser');
+let postResult = document.getElementById('postResult');
 
-const getData = async () => {
-    const response = await fetch(get);
-    data = await response.json();
-    console.log('data', data);
-    processing();
-};
-
-const processing = () => {
-    for (let x = 0; x < data.length; x++) {
-        drawing(data[x].name, data[x].id % 2 === 0);
+const funcPost = async () => {
+    try {
+        let payload = {
+            "id": parseInt(idUser.value) 
+        };
+        const req = { "method": 'POST', "headers": { 'Content-Type': 'application/json' }, "body": JSON.stringify(payload) };
+        let response = await fetch(post, req);
+        let data = await response.json();
+        let id = document.createElement('h2');
+        id.innerHTML = `ID: ${data[0].id}`;
+        postResult.appendChild(id);
+        let nameU = document.createElement('h2');
+        nameU.innerHTML = `NAME: ${data[0].name}`;
+        postResult.appendChild(nameU);
+        let user = document.createElement('h2');
+        user.innerHTML = `USER: ${data[0].user}`;
+        postResult.appendChild(user);
+        // createW(id, data[0]);
+        // createW(name, data[0]);
+        // createW(user, data[0]);
+    } catch (error) {
+        console.log(error);
     }
-};
-
-const drawing = (str, isOdd) => {
-    let h2 = document.createElement('h2');
-    h2.innerHTML = str;
-    isOdd ? odds.appendChild(h2) : evens.appendChild(h2);
-};
-
-getData();
+}
+// const createW = (pico, position) =>{
+//     let element = document.createElement('h2');
+//     let wea = pico.toString();
+//     wea.toUpperCase();
+//     element.innerHTML = `${wea}: ${position.pico}`;
+//     postResult.appendChild(element);
+// }
+send.addEventListener('click', funcPost);
