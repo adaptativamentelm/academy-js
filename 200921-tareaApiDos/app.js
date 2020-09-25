978cfcde78612387eca209f0b3141b5177f62216
AndreaMiranda1 <andrea@adaptativamente.cl>
2020-09-24 20:15:34 -0400

    ya luchito no me da el cerebro pa mas, podria hacer fors, quizas, pero kiero mimir uwu

--- 200921-tareaApiDos/app.js
+++ 200921-tareaApiDos/app.js
@@ -12,27 +12,17 @@ const funcPost = async () => {
         const req = { "method": 'POST', "headers": { 'Content-Type': 'application/json' }, "body": JSON.stringify(payload) };
         let response = await fetch(post, req);
         let data = await response.json();
-        let id = document.createElement('h2');
-        id.innerHTML = `ID: ${data[0].id}`;
-        postResult.appendChild(id);
-        let nameU = document.createElement('h2');
-        nameU.innerHTML = `NAME: ${data[0].name}`;
-        postResult.appendChild(nameU);
-        let user = document.createElement('h2');
-        user.innerHTML = `USER: ${data[0].user}`;
-        postResult.appendChild(user);
-        // createW(id, data[0]);
-        // createW(name, data[0]);
-        // createW(user, data[0]);
+        createW("id", data[0].id);
+        createW("name", data[0].name);
+        createW("user", data[0].user);
     } catch (error) {
         console.log(error);
     }
 }
-// const createW = (pico, position) =>{
-//     let element = document.createElement('h2');
-//     let wea = pico.toString();
-//     wea.toUpperCase();
-//     element.innerHTML = `${wea}: ${position.pico}`;
-//     postResult.appendChild(element);
-// }
+const createW = (str, position) =>{
+    let element = document.createElement('h2');
+    let title = str.toUpperCase();
+    element.innerHTML = `${title}: ${position}`;
+    postResult.appendChild(element);
+}
 send.addEventListener('click', funcPost);
