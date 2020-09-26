4cfb2ea159489a4907fbb6db01767a0c7bab5958
andrea castro <ap.castrocastellon@gmail.com>
2020-09-25 23:04:26 -0300

    tarea ApiDos terminada en 200925

--- 200921-tareaApiDos/app.js
+++ 200921-tareaApiDos/app.js
@@ -3,6 +3,9 @@ let post = 'https://portal-be.adaptativamente.cl/reporteria/academy/user/id';
 let data = [];
 let odds = document.getElementById('odd');
 let evens = document.getElementById('even');
+let catcher = document.getElementById('catcher');
+let input_get = document.getElementById('input_get');
+let finder_user = document.getElementById('finder_user');
 
 const getData = async () => {
     const response = await fetch(get);
@@ -22,5 +25,48 @@ const drawing = (str, isEven) => {
     h2.innerHTML = str;
     isEven ? evens.appendChild(h2) : odds.appendChild(h2);
 };
+//____________________________________________________________________
 
-getData();
+const getDataByPost = async (id) => {
+    // console.log(id)
+    try {
+        let payload = {
+            "id": id
+        };
+        const req = { "method": 'POST', "headers": { 'Content-Type': 'application/json' }, "body": JSON.stringify(payload) };
+        let response = await fetch(post, req);
+        let data = await response.json();
+        // draw_user_info(data);
+        console.log(data);
+        draw_user_info(data);    
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+const catchTheId = () => {
+    let find_id = Number(input_get.value);
+    // console.log(find_id);
+    getDataByPost(find_id);
+}
+
+
+const draw_user_info = (data) => {
+    // console.log(data[0].id);
+    // console.log(data[0].user);
+    let p1 = document.createElement('p');
+    let p2 = document.createElement('p');
+    let p3 = document.createElement('p');
+    p1.innerHTML = 'ID: ' + data[0].id;
+    p1.style.fontWeight = 'bold';
+    p2.innerHTML = 'NAME: '+ data[0].name;
+    p2.style.color = 'red';
+    p3.innerHTML = 'USER: ' + data[0].user;
+    p3.style.fontSize = '25px';
+    
+    finder_user.appendChild(p1);
+    finder_user.appendChild(p2);
+    finder_user.appendChild(p3);
+}
+
+catcher.addEventListener('click', catchTheId);
