88db2e5d8c2c37e5f5cb5c7ac02cd7ca50bd3d83
AndreaMiranda1 <andrea@adaptativamente.cl>
2020-09-28 20:08:36 -0400

    listo luchooo

--- 200925-tareaApiTres/app.js
+++ 200925-tareaApiTres/app.js
@@ -8,65 +8,54 @@ let data = [];
 let data2 = [];
 
 // obtiene data primera sec.
-// const getData = async () => {
-//     const response = await fetch(listUrl);
-//     data = await response.json();
-//     loop();
-// };
+const getData = async () => {
+    const response = await fetch(listUrl);
+    data = await response.json();
+    loop();
+};
 
-// // recorre la data para dibujarla
-// const loop = () => {
-//     for (let x = 0; x < data.length; x++) {
-//         draw(data[x].id, data[x].typeimg);    
-//     }
-// };
+// recorre la data para dibujarla
+const loop = () => {
+    for (let x = 0; x < data.length; x++) {
+        draw(data[x].id, data[x].typeimg);    
+    }
+};
 
 // // dibuja cada elemento
-// const draw = (id, img) => {
-//     let li = document.createElement('li');
-//     let image = document.createElement('img');
-//     li.innerHTML = id;
-//     li.style.fontSize = '1.5em';
-//     let licont = ulSec1.appendChild(li);
-//     image.src = `${img}`;
-//     image.style.width = '30%';
-//     image.style.display = 'block';
-//     licont.appendChild(image);
-//     let br = document.createElement('br');
-//     licont.appendChild(br);
-// };
+const draw = (id, img) => {
+    let li = document.createElement('li');
+    let image = document.createElement('img');
+    li.innerHTML = id;
+    li.style.fontSize = '1.5em';
+    let licont = ulSec1.appendChild(li);
+    image.src = `${img}`;
+    image.style.width = '30%';
+    image.style.display = 'block';
+    licont.appendChild(image);
+    let br = document.createElement('br');
+    licont.appendChild(br);
+};
 
 // --------------------------------------fin primera sec----------------------------------
 // obtiene data segunda sec.
 const getData2 = async () => {
     const response2 = await fetch(descUrl);
     data2 = await response2.json();
-    console.log(data2)
-    // loop2();
+    console.log(data2);
     let companyName = document.createElement('h2');
-    companyName.innerHTML = `${data2[1].company}`;
+    companyName.innerHTML = `Company: ${data2.company}`;
     section2.appendChild(companyName);
+    let nameN = document.createElement('h2');
+    nameN.innerHTML = `Name: ${data2.name}`;
+    section2.appendChild(nameN);
+    let date = document.createElement('h2');
+    date.innerHTML = `Date: ${data2.year}-${data2.month}-${data2.day}`;
+    section2.appendChild(date);
+    let image = document.createElement('img');
+    image.src = data2.img;
+    section2.appendChild(image);  
+  
 };
-// recorre data
-// const loop2 = () => {
-//     for (let y = 0; y < data2.length; y++) {
-//         draw2(data2[y].company)
-//         // draw2(data2[y].company, data2[y].name, data2[y].day, data2[y].month, data2[y].year, data2[y].img);    
-//     }
-// }
-// dibuja los elementos
-// const draw2 = (company, name, day, month, year, img) => {
-//     let companyName = document.createElement('h2');
-//     companyName.innerHTML = `Company: ${company}`;
-//     section2.appendChild(companyName);
-//     let nameN = document.createElement('h2');
-//     nameN.innerHTML = `Name: ${name}`;
-//     section2.appendChild(nameN);
-//     let date = document.createElement('h2');
-//     date.innerHTML = `Date: ${year}-${month}-${day}`
-//     let image = document.createElement('img');
-//     image.src = `${img}`;
-//     section2.appendChild(image);   
-// };
-// getData();
+
+getData();
 getData2();
