c56b355616ad810d4e36058b5afe43fb78a86779
Dani Olivares <dany.olivaresl@gmail.com>
2020-09-28 21:34:05 -0300

    tarea mitad dani_200925

--- 200925-tareaApiTres/app.js
+++ 200925-tareaApiTres/app.js
@@ -1,3 +1,38 @@
 const listUrl = 'https://portal-be.adaptativamente.cl/reporteria/academy/users-list';
 const descUrl = 'https://portal-be.adaptativamente.cl/reporteria/academy/description';
 const userByIdUrl = 'https://portal-be.adaptativamente.cl/reporteria/academy/user-complete/id';
+let data = [];
+let info = document.getElementById('info');
+
+const getList = async () => {
+    const response = await fetch(listUrl);
+    data = await response.json();
+    console.log('Dataaaaa' , data);
+    processing();
+};
+
+const processing = () => {
+    for ( let x = 0; x < data.length; x++) {
+        // console.log(data[x]);
+        drawing(data[x].id)
+        drawing2(data[x].typeimg);
+        // console.log(data[x].typeimg);
+    }
+};
+
+const drawing = (nro) => {
+    let li = document.createElement ('li');
+    li.innerHTML = nro;
+    info.appendChild(li);
+}
+
+const drawing2 = (i) => {
+    let imagen = new Image (100, 100);
+    imagen.src=i;
+    let img = document.createElement ('img');
+    img.innerHTML = imagen;
+    info.appendChild(imagen);
+}
+
+
+getList ();
