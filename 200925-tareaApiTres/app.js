49871f91ec339bf9d25a133674c2d41d34b45aa0
Dani Olivares <dany.olivaresl@gmail.com>
2020-10-05 22:56:53 -0300

    tarea api tres, segunda parte

--- 200925-tareaApiTres/app.js
+++ 200925-tareaApiTres/app.js
@@ -7,7 +7,7 @@ let info = document.getElementById('info');
 const getList = async () => {
     const response = await fetch(listUrl);
     data = await response.json();
-    console.log('Dataaaaa' , data);
+    // console.log('Dataaaaa' , data);
     processing();
 };
 
@@ -34,5 +34,28 @@ const drawing2 = (i) => {
     info.appendChild(imagen);
 }
 
+const getList2 = async () => {
+    const response = await fetch(descUrl);
+    data = await response.json();
+    console.log('DATA222222222222222',data);
+    console.log('0'+data.day);
+    let month = data.month;
+    if(month+1 > 9){
+         month = data.month+1;
+    }else{ '0'+(month=data.month++)
+    }
+    let day = data.day;
+    // if(data.day <10){
+    //     '0'+(day)
+    // }else{
+    //     day
+    // }
+    drawing(`Company : ${data.company}`);
+    drawing(`Nombre: ${data.name}`);
+    drawing(`Date: ${data.year}-${month}-${day <10 ? '0'+(data.day) : data.day }` )
+    drawing2(data.img);
+}
 
+
+getList2 ();
 getList ();
