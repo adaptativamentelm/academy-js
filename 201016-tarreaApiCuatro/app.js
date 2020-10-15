ff8dad2fb74aed394c68c7a467a912a6320e9e18
Luis Miranda <miranda.geldres@gmail.com>
2020-10-14 23:05:42 -0300

    tarea

--- 201016-tarreaApiCuatro/app.js
+++ 201016-tarreaApiCuatro/app.js
@@ -1,28 +1,3 @@
 let login = 'https://portal-be.adaptativamente.cl/reporteria/academy/login';
 let inputs = document.getElementsByTagName('input');
 let button = document.getElementById('button');
-
-const postLogin = async () => {
-    try {
-        const payload = {
-            "method": 'POST',
-            "headers": { 'Content-Type': 'application/json' },
-            "body": JSON.stringify({ "user": inputs[0].value, "pass": Number(inputs[1].value) })
-        };
-        const response = await fetch(login, payload);
-        let data = await response.json();
-        validate(data);
-    } catch (e) {
-        console.log(e);
-        validate([]);
-    }
-}
-
-const validate = (data) => {
-    if (data.length) {
-        localStorage.setItem('data', JSON.stringify(data[0]));
-        location.assign('one/one.html');
-    }
-}
-
-button.addEventListener('click', postLogin);
