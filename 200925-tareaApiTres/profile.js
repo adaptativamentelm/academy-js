c6daf55a7ce0df0fa428ea72c17b4b5e21379cdf
AndreaMiranda1 <andrea@adaptativamente.cl>
2020-10-14 22:01:31 -0400

    listoooo tarea terminada

--- 200925-tareaApiTres/profile.js
+++ 200925-tareaApiTres/profile.js
@@ -16,14 +16,27 @@ const catchId = (id) => {
 const getDataPost = async (url, payload, id) => {
     try{
         const response = await fetch(url, payload);
-        console.log('url',url);
-        console.log('payload',payload);
-        console.log('id',id);
+        // console.log('url',url);          
+        // console.log('id',id);
         dataPost = await response.json();
-        console.log('dataPost', dataPost);
-        let h2 = document.createElement('h2');
-        h2.innerHTML = id;
-        redirectPage.appendChild(h2);
+        console.log('dataPost', dataPost[0]);
+        let h2Id = document.createElement('h2');
+        h2Id.innerHTML = `ID: ${dataPost[0].id}`;
+        redirectPage.appendChild(h2Id);
+        let h2Name = document.createElement('h2');
+        h2Name.innerHTML = `NAME: ${dataPost[0].name}`;
+        redirectPage.appendChild(h2Name);
+        let h2User = document.createElement('h2');
+        h2User.innerHTML = `User: ${dataPost[0].user}`;
+        redirectPage.appendChild(h2User);
+        let h2Type = document.createElement('h2');
+        h2Type.innerHTML = `Rol: ${dataPost[0].typeid === 1 ?'Estudiante':'Profesor'}`;
+        redirectPage.appendChild(h2Type);
+        let imgUser = document.createElement('img');
+        imgUser.src = dataPost[0].img;
+        imgUser.style.width = '30em';
+        redirectPage.appendChild(imgUser);
+        
     }catch(error){
         console.log(error);
     }
