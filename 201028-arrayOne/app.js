9c2dc40cc3bf2a161e4722cfab5085e3c3dbcf2e
Luis Miranda <miranda.geldres@gmail.com>
2020-10-28 21:00:43 -0300

    avance

--- 201028-arrayOne/app.js
+++ 201028-arrayOne/app.js
@@ -1,1 +1,18 @@
 const getUrl = 'https://portal-be.adaptativamente.cl/reporteria/academy/users-list';
+let input = document.getElementById('input');
+let data = [];
+
+const getData = async (url) => {
+    const response = await fetch(url);
+    data = await response.json();
+    console.log('data', data);
+};
+
+const finding = () => {
+    console.log('input value', input.value);
+    let found = data.filter((el) => el.name.includes(input.value));
+    console.log(found);
+};
+
+getData(getUrl);
+input.addEventListener('keyup', finding);
