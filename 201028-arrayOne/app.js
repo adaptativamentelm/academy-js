05367b124202b4507ad39c2daf4a11083f31c4ec
chinchilllapsico <coraline.colous@gmail.com>
2020-11-02 18:46:27 -0400

    avances, luchito hoy la termino

--- 201028-arrayOne/app.js
+++ 201028-arrayOne/app.js
@@ -1,1 +1,35 @@
 const getUrl = 'https://portal-be.adaptativamente.cl/reporteria/academy/users-list';
+let inputNames = document.getElementById('inputNames');
+let data = [];
+let names = [];
+console.log('names', names);
+
+const getData = async () => {
+
+    try {
+        let response = await fetch(getUrl);
+        let data = await response.json();
+        console.log('data', data);
+        filterNames(data, inputNames.value.toUpperCase());
+        // console.log(inputNames.value.toUpperCase());
+        
+    } catch (e) {
+        console.log(e);
+    }
+
+}
+
+const filterNames = (info, valueName) => {
+
+    if (names.length===0) {
+        for (let i = 0; i < info.length; i++) {
+            names.push(info[i].name.toUpperCase());          
+        }
+        console.log('names despues del for', names);    
+    }
+    let filterArray = names.filter(e => e.includes(valueName)==true );
+    console.log(filterArray);
+}
+
+
+inputNames.addEventListener('keyup', getData);
