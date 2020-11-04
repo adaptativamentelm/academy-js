57979b793ef425d097a7b9df3a245babf8122b05
Luis Miranda <miranda.geldres@gmail.com>
2020-11-04 17:56:55 -0300

    Comentarios en example arrayTwo

--- 201028-arrayOne/app.js
+++ 201028-arrayOne/app.js
@@ -3,31 +3,32 @@ let input = document.getElementById('input');
 let main = document.getElementsByTagName('main')[0];
 let data = [];
 
-const getData = async (url) => {
+const getData = async (url) => { // Para obtener la información de la Api Rest
     const response = await fetch(url);
     data = await response.json();
+    console.log('data', data);
     processing(data);
 };
 
-const finding = () => {
-    deleteFromMain();
+const finding = () => { // Filtra los objetos en el arreglo
+    deleteFromMain(); // Hasta acá está vacío el main, solamente tiene el input
     let found = data.filter((el) => el.name.toLowerCase().includes(input.value.toLowerCase()));
     processing(found);
 };
 
-const processing = (arr) => {
+const processing = (arr) => { // Recorrido del arreglo
     arr.forEach((data) => {
         drawing(data);
     });
 };
 
-const drawing = (obj) => {
+const drawing = (obj) => { // Crea un h2, le escribe la propiedad name del objeto que recibe y agrego ese elemento al main
     let h2 = document.createElement('h2');
     h2.innerHTML = obj.name;
     main.appendChild(h2);
 };
 
-const deleteFromMain = () => {
+const deleteFromMain = () => { // Elimina todos los h2 del documento
     let h2s = document.getElementsByTagName('h2');
     for (let x = h2s.length - 1; x >= 0; x--) {
         h2s[x].remove();
