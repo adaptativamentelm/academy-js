27df642e4c54cd49b92c7227e9cd1cd855286266
andrea castro <ap.castrocastellon@gmail.com>
2020-11-04 16:09:59 -0300

    respaldo de tarea inconclusa

--- 201028-arrayOne/app.js
+++ 201028-arrayOne/app.js
@@ -5,7 +5,8 @@ let all_view = document.getElementById('all-view');
 
 // todos usuarios
 const getData = async () => {
-    let response = await fetch(getUrl);
+    let response = 
+    await fetch(getUrl);
     data = await response.json();
 };
 
@@ -13,22 +14,20 @@ const coincidencer = () => {
     let the_coincidence = (input.value).toLowerCase();
     // console.log(the_coincidence); 
     let filter_data = data.filter((data) => (data.name.toLowerCase()).includes(the_coincidence));
-    // console.log(filter_data);
-    print_data(filter_data);
-}
-
-const print_data = (filtered_data) => {
-    console.log(filtered_data);
+    console.log(filter_data);
     
-    filtered_data.forEach( data => {
-    });
-    
-    // esta cuesiton esta mala :v
-    if (filtered_data) {
+    filter_data.forEach( data => { // imprime por cada coincidencia
         let p = document.createElement('p');
         p.innerHTML = data.name;
         all_view.appendChild(p);
-    }
+        
+    });
+    
+    
+    
+    
+    
+    
     
     
 }
