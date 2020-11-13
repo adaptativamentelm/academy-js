1e766c072730bfe7e1249c89ea6a20e25b8c8ad5
Luis Miranda <miranda.geldres@gmail.com>
2020-11-13 14:12:29 -0300

    ejercicios realizados

--- 201111-ejerciciosPracticaArray/app.js
+++ 201111-ejerciciosPracticaArray/app.js
@@ -220,19 +220,10 @@
 
 
 
-
-
-
-
 // 8 Imprimir el índice correspondiente al elemento con valor 7
 // let data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 // console.log(data.indexOf(7));
-
-
-
-
-
 
 
 
@@ -247,12 +238,12 @@
 
 
 
-
-
-
 // 10 Imprimir el arreglo al revés y multiplicado por 3
 // let data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+// let dataMod = data.map((e) => e * 3).reverse();
+// let dataMod = data.reverse().map((e) => e * 3);
+// console.log(dataMod);
 
 
 
@@ -262,6 +253,15 @@
 // 11 Imprime la suma los elementos del arreglo multiplicado por 10
 // let data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+// console.log('reduce', data.reduce((acc, cv) => acc + cv));
+
+// const reducer1 = (acc, cv) => acc + cv;
+// console.log('reduce', data.reduce(reducer1));
+
+// const reducer = (accumulator, currentvalue) => accumulator + currentvalue;
+// let sum = data.reduce(reducer) * 10;
+// console.log('data con suma de todos los elementos *10', sum);
+
 
 
 
@@ -270,6 +270,8 @@
 // 12 Imprime el arreglo ordenado ascendentemente multiplicado por 2
 // let data = [0, 22, -1, 30, 45, -10, 10, 100, 70];
 
+// let data2 = data.sort((a, b) => a - b).map((e) => e * 2);
+// console.log('multiplicado', data2);
 
 
 
@@ -278,6 +280,9 @@
 
 // 13 Imprime el arreglo ordenado descendentemente multiplicado por 4
 // let data = [0, 22, -1, 30, 45, -10, 10, 100, 70];
+
+// let data2 = data.sort((a, b) => b - a).map((e) => e * 4);
+// console.log('multiplicado', data2);
 
 
 
@@ -316,9 +321,50 @@
 //     }
 // ];
 
+// data.shift();
+// console.log('despues*', data);
+
+// console.log('slice1', data.slice(1));
+// console.log('slice2', data.slice(1, 3));
+// console.log('data', data);
 
 
 
 
-// 15 Usando el arreglo creado crear una función donde ingresas día y mes y te retorne el acumulado de cantidad días desde el primero de enero hasta la fecha ingresada
-// const data = [31, 28.25, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+// 15 Usando el arreglo crear una función donde ingresas día y mes, y te retorne el acumulado de cantidad días
+// desde el primero de enero hasta la fecha ingresada
+// const data_dias = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+// const funcion_pedida1 = (dia, mes) => {
+//     let seleccion = mes > 1 ? data_dias.slice(0, m) : [0];
+//     const reducer1 = (acc, cv) => acc + cv;
+//     // console.log('reduce', seleccion.reduce(reducer1));
+//     let suma_mes = seleccion.reduce(reducer1);
+//     let incluye_dias = suma_mes + dia;
+//     console.log(incluye_dias);
+//     return incluye_dias;
+
+// };
+
+// const funcion_pedida2 = (dia, mes) => {
+    
+//     let nuevo =data_dias.slice(0, mes - 1).reduce((a, b) => a + b) + dia;
+    
+//     return nuevo;
+    
+// };
+
+
+
+// const funcion_pedida3 = (dia, mes) => {
+    
+//     return mes > 1 ? data_dias.slice(0, mes - 1).reduce((acc, cv) => acc + cv) + dia : 0 + dia;
+    
+// };
+
+
+// console.log('resultado fp2', funcion_pedida1(1, 1));
+
+// console.log('resultado fp2', funcion_pedida2(31, 12));
+
+// console.log('resultado fp3', funcion_pedida3(2, 3));
