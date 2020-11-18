d8c44f680acb8a5769dc6f6a0c84a798f9759e9c
zwiva <ap.castrocastellon@gmail.com>
2020-11-18 18:43:26 -0300

    primer acercamiento calculadora sin investigar

---
+++ 201113-calculadoraOne/app.js
@@ -0,0 +1,22 @@
+console.log('estamos dentro');
+let the_input = document.getElementById('the_input');
+let add_btn = document.getElementById('add_btn');
+let sub_btn = document.getElementById('sub_btn');
+
+
+
+const add = () => {
+
+}
+const substract = () => {
+
+}
+
+console.log(the_input.value);
+
+const multiply = () => {
+    
+}
+const divide = () => {
+    
+}
