52c27a96a3bcd893328eb453900cb752b3281de4
NataliaSaavedraM <nataliasaavedram@gmail.com>
2020-11-18 22:10:44 -0300

    Calculadora suma y resta con css basico

--- 201113-calculadoraOne/app.js
+++ 201113-calculadoraOne/app.js
@@ -1,50 +1,75 @@
-
-let sections = document.getElementsByTagName("section");
-let h2 = document.createElement("h2");
-//let buttonEquals = document.getElementById("equal");
-//const positionButton = () => {
-let button = document.getElementsByTagName("button");
-
-//let buttonClear=document.getElementById('clean');
-//let buttonAdd=document.getElementById('summ');
-// let buttonMinus=document.getElementById('minus')
-//let displayed = false;
-
-let valueButton = [1, 2, 3, "+", 4, 5, 6, "-", 7, 8, 9, "=", 0, ""];
-
-let num = [];
-
-for (let i = 0; i < button.length; i++) {
-  button[i].addEventListener("click", () => {
-    if (
-      valueButton.indexOf(1) ||
-      valueButton.indexOf(2) ||
-      valueButton.indexOf(3) ||
-      valueButton.indexOf(4) ||
-      valueButton.indexOf(5) ||
-      valueButton.indexOf(6) ||
-      valueButton.indexOf(7) ||
-      valueButton.indexOf(8) ||
-      valueButton.indexOf(9) ||
-      valueButton.indexOf(0)
-    ) {
-      //   console.log(valueButton[i]);
-      num.push(valueButton[i]);
-      let x=num.join('')
-      console.log(x);
-      
-    } 
+const buttonNumber = document.getElementsByName("number");
+const buttonOperation = document.getElementsByName("op");
+const buttonEqual = document.getElementsByName("equal");
+const buttonClear = document.getElementsByName("clean");
+const screen = document.getElementById("screen");
+let opActually = '';
+let opBefore = '';
+let operation = undefined;
+//console.log(buttonClear[0])
+buttonNumber.forEach((button) => {
+  button.addEventListener("click", () => {
+    addNumber(button.innerText);
   });
-}
+});
 
+buttonOperation.forEach((button) => {
+  button.addEventListener("click", () => {
+    selectOperation(button.innerText);
+  });
+});
+buttonEqual[0].addEventListener("click", () => {
+  calculator();
+  actuallyScreen();
+}); // Igual
 
-//positionButton();
-//const num1,num2;
-const sum = (num1, num2) => {
-  return num1 + num2;
+buttonClear[0].addEventListener("click", () => {
+  clear();
+  actuallyScreen();
+});
+
+const selectOperation = (op) => {
+  if (opActually === '') return;
+  if (opBefore !== '') {
+    calculator();
+  }
+  operation = op.toString();
+  opBefore = opActually;
+  opActually = '';
 };
-const rest = (num1, num2) => {
-  return num1 - num2;
+
+const calculator = () => {
+  let cal;
+ let before = parseFloat(opBefore);
+  let actually = parseFloat(opActually);
+  if (isNaN(before) || isNaN(actually)) return;
+  switch(operation){
+    case "+":
+      cal = before + actually;
+      break;
+    case "-":
+      cal = before - actually;
+      break;
+    default:
+      return;
+  }
+  opActually=cal;
+  operation=undefined;
+  opBefore='';
 };
-console.log(sum(52,25))
-console.log(rest(52,32))
+
+const addNumber = (num) => {
+  opActually = opActually.toString() + num.toString();
+  actuallyScreen();
+  console.log(opActually);
+};
+
+const clear = () => {
+  opActually = '';
+  opBefore = '';
+  operation = undefined;
+};
+
+const actuallyScreen = () => {
+  screen.value = opActually;
+};
