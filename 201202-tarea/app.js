ea69b49571dbc141b79471a7b16fc3f54a137bf1
Andrea Castro <ap.castrocastellon@gmail.com>
2020-12-02 18:35:37 -0300

    aqui esta la tarea

--- 201202-tarea/app.js
+++ 201202-tarea/app.js
@@ -1,2 +1,31 @@
 let urlGet = 'https://portal-be.adaptativamente.cl/reporteria/academy/get-user/id/';
 let qty = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let btn = document.getElementById('btn');
+let data = [];
+let students = [];
+let students_section = document.getElementById('students_section');
+
+const getData = async (url) => {
+    let response = await fetch(url);
+    data = await response.json();
+    filterStudents(data);
+}
+qty.forEach(element => {    
+    let url = `${urlGet}${element}`;
+    getData(url);
+});
+const filterStudents = (data) => {
+    if (data[0].typename == "student") {
+        students.push(data[0].name)
+    }
+}
+const printStudents = () => {
+    students.forEach(elem => {
+        let h3 = document.createElement('h3');
+
+        h3.innerHTML = elem;
+        students_section.appendChild(h3);
+    })
+}
+
+btn.addEventListener('click', printStudents )
