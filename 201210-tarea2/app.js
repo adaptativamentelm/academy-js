3d786dba0a6040cb30639a2646c2b503545461e7
Igor <igor@adaptativamente.cl>
2020-12-14 09:36:29 -0300

    Tarea 201210 (Falta marco)

--- 201210-tarea2/app.js
+++ 201210-tarea2/app.js
@@ -1,2 +1,39 @@
 let urlGet = 'https://portal-be.adaptativamente.cl/reporteria/academy/get-user/id/';
 let qty = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let printPosition = document.getElementById('Here');
+
+const getData = async(id) => {
+    let url = `${urlGet}${id}`
+    let dataFetch = await fetch(url);
+    let dataReady = await dataFetch.json();
+    return dataReady
+}
+
+const drawHtml = (htmlTag, htmlParent, id) => {
+    let eleName = document.createElement(htmlTag);
+    eleName.id = id;
+    htmlParent.appendChild(eleName);
+    return eleName;
+}
+
+const printData = async() => {
+    for (let i = 1; i <= qty.length; i++) {
+        let dataUser = await getData(i);
+        let tagName = drawHtml('h2', printPosition, dataUser[0]["name"]);
+        let imgUser = new Image(100, 100);
+        imgUser.src = dataUser[0]['img'];
+        tagName.innerHTML = dataUser[0]["name"] + "<br>";
+        tagName.appendChild(imgUser);
+        let deleteButton = drawHtml('button', tagName, `user${i}`);
+        deleteButton.innerHTML = "Eliminar";
+        deleteButton.setAttribute("onclick", onclick = "deletePrint(this)"); //para que el boton elimine su propio div que lo contiene.
+    };
+}
+
+
+const deletePrint = (divEle) => {
+    divEle.parentElement.remove();
+    console.log(`Se eliminó el usuario ${divEle.parentElement.id}`);
+}
+
+printData();
