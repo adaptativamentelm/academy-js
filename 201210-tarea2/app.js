f345fe07e361fd58b337b82553889ad30e25f8c5
AndreaMiranda1 <53183334+AndreaMiranda1@users.noreply.github.com>
2020-12-17 16:39:06 -0400

    avance html y js, falta boton borrar

--- 201210-tarea2/app.js
+++ 201210-tarea2/app.js
@@ -1,2 +1,41 @@
 let urlGet = 'https://portal-be.adaptativamente.cl/reporteria/academy/get-user/id/';
 let qty = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let button = document.getElementById('button');
+let divContainer = document.getElementById('divContainer');
+let data = [];
+
+const show = async () => {
+    for (let x = 0; x < qty.length; x++) {
+        let user = await getData(`${urlGet}${qty[x]}`);
+        data.push(user[0]);
+    }
+    draw(data);
+    console.log(data);
+};
+
+const getData = async (url) => {
+    const response = await fetch(url);
+    return await response.json();
+};
+
+const draw = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+            let h2 = document.createElement('h2');
+            let h3 = document.createElement('h3');
+            let user = document.createElement('h3');
+            let img = document.createElement('img');
+            let buttonDiv = document.createElement('button');
+            img.src = arr[i].img;
+            h2.innerHTML = arr[i].name;
+            h3.innerHTML = arr[i].id;
+            user.innerHTML = `User: ${arr[i].user}`;
+            buttonDiv.innerHTML = 'borrar';
+            divContainer.appendChild(h2);
+            divContainer.appendChild(h3);
+            divContainer.appendChild(user);
+            divContainer.appendChild(img);
+            divContainer.appendChild(buttonDiv);
+    }
+};
+
+button.addEventListener('click', show);
