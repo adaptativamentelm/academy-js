2d6e6a604ab8035bbbd7c978903c1142f8896145
scarplaton <51921205+scarplaton@users.noreply.github.com>
2021-01-11 18:24:16 -0300

    adelanto tarea css scar

--- 201202-tarea/app.js
+++ 201202-tarea/app.js
@@ -1,2 +1,15 @@
 let urlGet = 'https://portal-be.adaptativamente.cl/reporteria/academy/get-user/id/';
 let qty = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let datos = document.getElementById('datos')
+
+// obtencion de datos
+const getData = async () => {
+    try {
+        let response = await fetch(urlGet);
+        let data = await response.json();
+    } catch (e) {
+        console.log(e);
+    }
+    console.log(data)
+}
+getData();
